596a22b8b9c24769d0dc1002e7a3f90a06915976
findlaysmith <ferelach45@gmail.com>
2017-10-12 15:46:40 +0100

    added path() receiver, having problems with rounding

--- simulation.go
+++ simulation.go
@@ -6,6 +6,7 @@ import (
 )
 
 type Particle struct {
+	//group particle variables together
 	initialHeight, initialVelocity, finalHeight, finalVelocity, maxHeight, horizontalRange, flightTime, theta, g float64
 }
 
@@ -31,11 +32,29 @@ func (p *Particle) maxRange() {
 	p.horizontalRange = (p.initialVelocity * math.Cos(p.theta)) * p.flightTime
 }
 
+func (p Particle) position(x float64) float64 {
+	y := (x * math.Tan(p.theta)) - ((p.g * math.Pow(x, 2)) / (2 * math.Pow(p.initialVelocity, 2) * math.Pow((math.Cos(p.theta)), 2))) + p.initialHeight
+	return y
+}
+
+func (p Particle) path() ([]float64, []float64) {
+	var xs []float64
+	var ys []float64
+	for x := 0.0; x < p.horizontalRange; x += 0.1 {
+		xs = append(xs, x)
+		ys = append(ys, p.position(x))
+	}
+	return xs, ys
+}
+
 func main() {
 	yerMaw := Particle{initialVelocity: 10.0, theta: 45.0, g: 9.8}
 	yerMaw.thetaDegrees()
 	yerMaw.maximumHeight()
 	yerMaw.timeOfFlight()
 	yerMaw.maxRange()
-	fmt.Println(yerMaw.maxHeight, yerMaw.flightTime, yerMaw.horizontalRange)
+	x, y := yerMaw.path()
+	fmt.Println(x)
+	fmt.Println("\n\n\n\n")
+	fmt.Println(y)
 }
