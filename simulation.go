50771c66d57f1c8b1e787054b9e38f67789e5165
findlaysmith <ferelach45@gmail.com>
2017-10-12 12:59:36 +0100

    added timeOfFlight, subsequently broke MaximumHeight

--- simulation.go
+++ simulation.go
@@ -6,17 +6,29 @@ import (
 )
 
 type Particle struct {
-	initialHeight, initialVelocity, finalHeight, finalVelocity, maxHeight, horizontalRange, flightTime, theta float64
+	initialHeight, initialVelocity, finalHeight, finalVelocity, maxHeight, horizontalRange, flightTime, theta, g float64
 }
 
-func (p *Particle) max_height() {
-	g := 9.8
+func (p *Particle) thetaDegrees() {
 	p.theta = (p.theta / 360) * 2 * math.Pi
-	p.maxHeight = ((p.initialVelocity*math.Sin(p.theta))*(p.initialVelocity*math.Sin(p.theta)))/(2*g) + p.initialHeight
+}
+
+func (p *Particle) maximumHeight() {
+	p.maxHeight = (math.Pow((p.initialVelocity*math.Sin(p.theta)), 2))/(2*p.g) + p.initialHeight
+}
+
+func (p *Particle) timeOfFlight() {
+	a := math.Pow((p.initialHeight * math.Sin(p.theta)), 2)
+	b := 2 * p.g * p.initialHeight
+	c := a + b
+
+	p.flightTime = (math.Sqrt(c) + (p.initialHeight * math.Sin(p.theta))) / p.g
 }
 
 func main() {
-	yerMaw := Particle{initialVelocity: 10.0, theta: 45.0, initialHeight: 0.0}
-	yerMaw.max_height()
-	fmt.Println(yerMaw.maxHeight)
+	yerMaw := Particle{initialVelocity: 10.0, theta: 45.0, initialHeight: 10.0, g: 9.8}
+	yerMaw.thetaDegrees()
+	yerMaw.maximumHeight()
+	yerMaw.timeOfFlight()
+	fmt.Println(yerMaw.maxHeight, yerMaw.flightTime)
 }
